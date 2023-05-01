6e272797bac6e74e888b87f3d1f9a85e9d3606e6
Sebastian Maldonado <seb_cep_12@hotmail.com>
2023-05-01 00:58:08 -0300

    update

--- Walter2023/app.py
+++ Walter2023/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, flash
+from flask import Flask, request, render_template
 from conexion import (conect_to_db, 
                       db_params,
                       get_all_persons,
