a4d84c409e4f96cd029cd8d00cb91daf3b332bde
Sebastian Maldonado <58012659+curtistyle@users.noreply.github.com>
2023-09-14 16:50:54 -0300

    update initial repo

--- JSON/drive_json/methods.py
+++ JSON/drive_json/methods.py
@@ -1,22 +1,42 @@
 import json
+from os import remove,close
 
-def crear_archivo(name : str):
-    name = name + '.json'
-    try: 
-        open(name, 'x')
-    except FileExistsError:
-        print("El archivo ya existe")
+def crear_archivo(name : str, extension='.json'):
+    name = name + extension 
+    """ (w) abierto para escritura"""
+    fichero = open(name, 'w', errors='namereplace')
+    fichero.close()
+    return fichero, name
+
+def abrir_archivo(name : str):
+    """ (r) abierto para lectura"""
+    fichero = open(name,'r')
+    return fichero
     
+
+def borrar_archivo(name : str):
+    try:
+        abrir_archivo(name)
+        remove(name)
+        print("El fichero fue borrado.")
+    except FileNotFoundError:
+        print("El fichero no existe.")
+        
+    
+
+    
+
     # if open(name, 'r') is FileNotFoundError:
     #     f = open(name,'w')
     # else:
     #     print("El archivo existe papu")
     # return f
 
-def abrir_archivo(name :str):
-    pass
 
 if __name__=='__main__':
-    crear_archivo("banda")
+    #abrir_archivo('ASD.json')
+    
+    
+    borrar_archivo('banda.json')
     
     
---
+++ JSON/readme.md
@@ -0,0 +1,14 @@
+# Ficheros
+
+## `open()`
+
+| Caracter  | Significado |
+| ------------- | ------------- |
+| `r`  | Abierto para lectura, *por defecto*  |
+| `w`  | Abierto para escritura, *truncando primero el fichero* |
+| `x`  | Abierto para creacion en exclisiva, *falla si el fichero ya existe* |
+| `a`  | Abierto para escritura, *añadiendo al final del fichero si existe* |
+| `b`  | modo binario |
+| `t`  | modo texto *(por defecto)* |
+| `+`  | abierto para actualizar *(lectura y escritura)* |
+
