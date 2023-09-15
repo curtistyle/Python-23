85def4432f8e08e30bbe1396fca582dd3d7a1de9
Sebastian Maldonado <58012659+curtistyle@users.noreply.github.com>
2023-09-15 01:17:31 -0300

    Update methods.py

--- JSON/drive_json/methods.py
+++ JSON/drive_json/methods.py
@@ -6,13 +6,18 @@ def crear_archivo(name : str, extension='.json'):
     """ (w) abierto para escritura"""
     fichero = open(name, 'w', errors='namereplace')
     fichero.close()
-    return fichero, name
+    return name
 
 def abrir_archivo(name : str):
     """ (r) abierto para lectura"""
     fichero = open(name,'r')
     return fichero
     
+def escribir_archivo(name : str):
+    """ (a) abierto para escritura """
+    fichero = open(name, 'a')
+    return fichero
+
 
 def borrar_archivo(name : str):
     try:
@@ -21,8 +26,16 @@ def borrar_archivo(name : str):
         print("El fichero fue borrado.")
     except FileNotFoundError:
         print("El fichero no existe.")
-        
     
+def json_a_archivo(dictionary : dict, name : str):
+    try:
+        file = escribir_archivo(name)
+    except FileExistsError:
+        print("El archivo no existe")
+        crear_archivo(name)
+        file = escribir_archivo(name)
+    with file as value:
+        json.dump(dictionary)
 
     
 
@@ -34,9 +47,47 @@ def borrar_archivo(name : str):
 
 
 if __name__=='__main__':
-    #abrir_archivo('ASD.json')
+    # abrir_archivo('ASD.json')
+    # borrar_archivo('banda.json')
     
-    
-    borrar_archivo('banda.json')
-    
-    
+    banda = {
+        "banda" : [
+            {
+                "title"  : "The Offspring",
+                "list"   : 1,
+                "albums" : [
+                    {
+                        "title"      : "Americana",
+                        "year"       : "1998",
+                        "track_list" : [
+                            {
+                                "title"  : "Have You Ever",
+                                "time"   : "03:50",
+                                "number" : 2
+                            },
+                            {
+                                "title"  : "Staring At The Sun",
+                                "time"   : "02:13",
+                                "number" : 3
+                            }
+                        ] 
+                    },
+                    {
+                        "title"      : "Splinter",
+                        "year"       : "2003",
+                        "track_list" : [
+                            {
+                                "title"  : "Necon",
+                                "time"   : "3:18",
+                                "number" : 1
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+
+    }
+
+
+    json_a_archivo(banda,'banda')
